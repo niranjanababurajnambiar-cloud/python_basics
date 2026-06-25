c5766d6b8e2759c30cc5f70004af36bcecfbfb68
niranjanababurajnambiar-cloud <niranjanababurajnambiar.com>
2026-06-25 16:55:19 +0530

    python_basics

---
+++ test.py
@@ -0,0 +1,24 @@
+print("hello world","world",sep="#")
+print("hello world",end="")
+print("hello world")  
+x="10.5"
+y="s"
+print("abc">"abd")
+x = input("Enter something: ")
+print("You entered:", x)
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      my name is
+      john
+      """)
+x="hello"
+print(x[0:3:2])
+print(x[::-1])
+l=[1,2,3,4,5]
+print(l[::2])
+for i in range(0,10,2):
+    print(i)
+while True:
+    print("hello")
